cc111f5e7695d2d7e37081c1b71b06a88c485141
CleytonPinheiro <cleyton_pin@hotmail.com>
2019-12-10 22:45:05 -0300

    Implementando método exclusão

--- public/index.php
+++ public/index.php
@@ -2,28 +2,24 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Alura\Cursos\Controller\InterfaceControladorRequisicao;
+use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\ListarCursos;
+use Alura\Cursos\Controller\Persistencia;
 
-
-
-$caminho = $_SERVER['PATH_INFO'];
-$rotas = require __DIR__ . '/../config/routes.php';
-
-if (!array_key_exists($caminho, $rotas)){
-    http_response_cod (404);
-    echo "Erro 404";
-    exit();
+switch ($_SERVER['PATH_INFO']) {
+    case '/listar-cursos':
+        $controlador = new ListarCursos();
+        $controlador->processaRequisicao();
+        break;
+    case '/novo-curso':
+        $controlador = new FormularioInsercao();
+        $controlador->processaRequisicao();
+        break;
+    case '/salvar-curso':
+        $controlador = new Persistencia();
+        $controlador->processaRequisicao();
+        break;
+    default:
+        echo "Erro 404";
+        break;
 }
-
-$classeControladora = $rotas[$caminho];
-/**@var InterfaceControladorRequisicao $controlador */
-$controlador = new $classeControladora();
-$controlador->processaRequisicao();
-
-
-
-
-
-
-
-
