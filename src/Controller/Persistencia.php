04c1c7eedb1f39c117f18fba7aa39c9cc38eae97
CleytonPinheiro <cleyton_pin@hotmail.com>
2019-12-10 22:57:53 -0300

    Ajustes

--- src/Controller/Persistencia.php
+++ src/Controller/Persistencia.php
@@ -15,24 +15,36 @@ class Persistencia implements InterfaceControladorRequisicao
 
     public function __construct()
     {
-
-    $this-> entityManager = (new EntityManagerCreator())
-        ->getEntityManager();
+        $this->entityManager = (new EntityManagerCreator())
+            ->getEntityManager();
     }
 
     public function processaRequisicao(): void
     {
-        $descricao = filter_input(INPUT_POST,
-            'descricao', FILTER_SANITIZE_STRING);
+        $descricao = filter_input(
+            INPUT_POST,
+            'descricao',
+            FILTER_SANITIZE_STRING
+        );
 
-        $curso= new Curso();
-        $curso->setDescricao($_POST ['descricao']);
-        $this->entityManager->persist($curso);
+        $curso = new Curso();
+        $curso->setDescricao($descricao);
+
+        $id = filter_input(
+            INPUT_GET,
+            'id',
+            FILTER_VALIDATE_INT
+        );
+
+        if (!is_null($id) && $id !== false) {
+            $curso->setId($id);
+            $this->entityManager->merge($curso);
+        } else {
+            $this->entityManager->persist($curso);
+        }
+
         $this->entityManager->flush();
 
-
-        header('Location: /listar-cursos', false, 302);
-
-
+        header('Location: /listar-cursos', true, 302);
     }
 }
